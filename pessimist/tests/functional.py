9f42cf430b20f7de32ab59cfd5d54a36e3ca5038
Tim Hatch <tim@timhatch.com>
2020-10-03 14:05:00 -0700

    Reduce parallelism in tests

--- pessimist/tests/functional.py
+++ pessimist/tests/functional.py
@@ -9,7 +9,7 @@ class FunctionalTest(unittest.TestCase):
     def test_project1(self) -> None:
         # TODO unmingle stderr
         runner = CliRunner()
-        result = runner.invoke(main, ["test_data/project1"],)
+        result = runner.invoke(main, ["test_data/project1", "-p", "2"],)
         print(result.exit_code)
         print(result.output)
         self.assertEqual(0, result.exit_code)
